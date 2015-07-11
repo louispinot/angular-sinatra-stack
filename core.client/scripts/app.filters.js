7d25d2aa06bb15b953b2cf54bfc6ae42f0885a46
Louis Pinot <lpvillechenon@gmail.com>
2015-07-11 12:05:34 +0200

    disabling some config scripts

--- core.client/scripts/app.filters.js
+++ core.client/scripts/app.filters.js
@@ -1,36 +1,36 @@
-(function(){
-  angular.module('habitac').run(['$rootScope', '$location',  'loginService', auth_redirect]);
-  angular.module('habitac').run(['$rootScope', '$location',  'loginService', logout]);
-  angular.module('habitac').run(['$rootScope', set_layout_variable]);
-  angular.module('habitac').run(['$rootScope', 'alertService', clear_alerts]);
+// (function(){
+  // angular.module('habitac').run(['$rootScope', '$location',  'loginService', auth_redirect]);
+  // angular.module('habitac').run(['$rootScope', '$location',  'loginService', logout]);
+  // angular.module('habitac').run(['$rootScope', set_layout_variable]);
+  // angular.module('habitac').run(['$rootScope', 'alertService', clear_alerts]);
 
-  function auth_redirect($rootScope, $location, loginService) {
-    $rootScope.$on('$routeChangeStart', function (event, next) {
-      if(!loginService.isAuthenticated() && next.authenticated) {
-        loginService.logout();
-        $location.path('/login');
-      }
-    });
-  }
+  // function auth_redirect($rootScope, $location, loginService) {
+  //   $rootScope.$on('$routeChangeStart', function (event, next) {
+  //     if(!loginService.isAuthenticated() && next.authenticated) {
+  //       loginService.logout();
+  //       $location.path('/login');
+  //     }
+  //   });
+  // }
 
-  function logout($rootScope, $location, loginService) {
-    $rootScope.$on('$routeChangeStart', function (event, next) {
-      if(next.logoutPage) {
-        loginService.logout();
-        $location.path('/login');
-      }
-    });
-  }
+  // function logout($rootScope, $location, loginService) {
+  //   $rootScope.$on('$routeChangeStart', function (event, next) {
+  //     if(next.logoutPage) {
+  //       loginService.logout();
+  //       $location.path('/login');
+  //     }
+  //   });
+  // }
 
-  function set_layout_variable($rootScope) {
-    $rootScope.$on('$routeChangeStart', function (event, next) {
-      $rootScope.layout = next.layout;
-    });
-  }
+  // function set_layout_variable($rootScope) {
+  //   $rootScope.$on('$routeChangeStart', function (event, next) {
+  //     $rootScope.layout = next.layout;
+  //   });
+  // }
 
-  function clear_alerts($rootScope, alertService){
-    $rootScope.$on('$routeChangeStart', function () {
-      alertService.clear();
-    });
-  }
-})();
+  // function clear_alerts($rootScope, alertService){
+  //   $rootScope.$on('$routeChangeStart', function () {
+  //     alertService.clear();
+  //   });
+  // }
+// })();
--- core.client/scripts/app.js
+++ core.client/scripts/app.js
@@ -4,19 +4,19 @@
   angular.module('habitac', ['ngRoute', 'angular-loading-bar', 'ngAnimate','ngResource', 'LocalStorageModule', 'ui.bootstrap'])
 
 
-  angular.module('habitac').run(['$http','sessionService', 'userService', set_header]);
+  // angular.module('habitac').run(['$http','sessionService', 'userService', set_header]);
 
-  function set_header($http, sessionService, userService){
-    //   /!\ ca a priori on ca s'en reservir (set_header($http, sessionService, userService))
+  // function set_header($http, sessionService, userService){
+  //   //   /!\ ca a priori on ca s'en reservir (set_header($http, sessionService, userService))
 
-    var currentSession = sessionService.getCurrentSession();
-    if(currentSession){
-      // get the user again if they already have a session. Prevents issues that result if we change the format of local storage.
-      $http.defaults.headers.common['X-Api-SessionToken'] = currentSession.session_token;
-      userService.getUser().then(function(user) {
-        userService.setUser(user);
-      });
-    }
-  }
+  //   var currentSession = sessionService.getCurrentSession();
+  //   if(currentSession){
+  //     // get the user again if they already have a session. Prevents issues that result if we change the format of local storage.
+  //     $http.defaults.headers.common['X-Api-SessionToken'] = currentSession.session_token;
+  //     userService.getUser().then(function(user) {
+  //       userService.setUser(user);
+  //     });
+  //   }
+  // }
 
 })();
